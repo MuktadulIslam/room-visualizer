cfe87aca71759455c93b10069e215ae8e722b001
Muktadul Islam <76801029+MuktadulIslam@users.noreply.github.com>
2025-08-03 18:29:56 +0600

    Update the lighting

--- src/components/Lighting.tsx
+++ src/components/Lighting.tsx
@@ -5,7 +5,7 @@ export default function Lighting() {
   return (
     <>
       {/* Ambient light for overall illumination */}
-      <ambientLight intensity={0.5} />
+      <ambientLight intensity={1} />
       
       {/* Point light in the center of the room */}
       <pointLight position={[0, 0, 0]} intensity={1} />
@@ -13,7 +13,7 @@ export default function Lighting() {
       {/* Additional directional light for better visibility */}
       <directionalLight
         position={[5, 5, 5]}
-        intensity={0.5}
+        intensity={1}
         castShadow
         color="#ffffff"
       />
